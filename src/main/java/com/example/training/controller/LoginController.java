0dc324f2a25b7d1321c52d5e01f2e95c5663cb50
nate <lichengwang@tist.com.tw>
2023-08-21 17:02:11 +0800

    commit

--- src/main/java/com/example/training/controller/LoginController.java
+++ src/main/java/com/example/training/controller/LoginController.java
@@ -1,17 +1,13 @@
 package com.example.training.controller;
 
 import com.example.training.domain.Mq;
-import com.example.training.domain.User;
 import com.example.training.service.UserService;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
-import java.util.Objects;
 
 @Controller
 @RequestMapping("/admin")
@@ -28,18 +24,8 @@ public class LoginController {
 
     @GetMapping("")
     public String index() {
-        return VIEW_LOGIN;
-    }
-
-    @PostMapping("/login")
-    public String login(@ModelAttribute User user) {
         rabbitTemplate.convertAndSend("login.queue", new Mq("Login"));
-        if (Objects.isNull(userService.findByAccount(user.getUsername()))) {
-            return VIEW_LOGIN;
-        } else {
-            return VIEW_INDEX;
-        }
-
+        return VIEW_LOGIN;
     }
 
 }
